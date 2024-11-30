ddac988bd8007fefb9400ff53d4e82ef36c40ae6
Faith Wanjiku <wanjikuf141@gmail.com>
2024-12-01 01:27:12 +0300

    commit

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+// Multiply
+const num1 = 31; 
+const num2 = 2;
+const multiply = num1 * num2;
+
+// Random
+const random = Math.floor(Math.random() * 100) + 1;
+
+// Mod
+const num3 = 28;
+const num4 = 6;
+const mod = num3 % num4;
+
+// Max
+const max = Math.max(1, 5, 10, 20);
